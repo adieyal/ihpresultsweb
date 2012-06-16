58917a255678e4687b9f8f35755a7117829e52cd
Adi Eyal <adi@burgercom.co.za>
2012-06-16 20:47:01 +0200

    Added format thousands

--- ihp/media/js/countryscorecard-d3.js
+++ ihp/media/js/countryscorecard-d3.js
@@ -1,3 +1,19 @@
+if (typeof Number.prototype.formatThousands != 'function') {
+    Number.prototype.formatThousands = function(c, d, t) {
+        var n = this,
+            c = isNaN(c = Math.abs(c)) ? 2 : c,
+            d = d == undefined ? "." : d,
+            t = t == undefined ? "," : t,
+            s = n < 0 ? "-" : "",
+            i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+            j = (j = i.length) > 3 ? j % 3 : 0;
+       return s
+              + (j ? i.substr(0, j) + t : "")
+              + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t)
+              + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+    }
+}
+
 var fill_svg = function(json){
     var n, i;
     d3.selectAll(".remove").remove();
@@ -48,7 +64,7 @@ var fill_svg = function(json){
 
         if (n.node() !== null){
             data = json.health_systems[i];
-            n.select('.value').text(data.value);
+            n.select('.value').text(data.value.formatThousands(1));
 
             var g = n.select('.graph');
             g.select('.point');
