3f69a7389e4a664ab993af8491288d61134e2697
Adi Eyal <adi@burgercom.co.za>
2012-06-17 21:25:35 +0200

    Added government questions to create blanks

--- ihp/import2012/management/commands/create_blanks.py
+++ ihp/import2012/management/commands/create_blanks.py
@@ -13,13 +13,25 @@ class Command(BaseCommand):
 
     @transaction.commit_on_success
     def handle(self, *args, **options):
-        questions = [
+        dp_questions = [
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
             "11", "12", "13", "14", "15", "16", "17", "18", "10old", "11old",
         ]
-        for submission in Submission.objects.all():
-            for question_number in questions:
+
+        gov_questions = [str(q) for q in range(28)]
+
+        for submission in Submission.objects.filter(type=Submission.DP):
+            for question_number in dp_questions:
                 _, created = DPQuestion.objects.get_or_create(
+                    submission=submission,
+                    question_number=question_number
+                )
+                if created:
+                    print "Created %s for %s in %s" % (question_number, submission.agency, submission.country)
+
+        for submission in Submission.objects.filter(type=Submission.Gov):
+            for question_number in gov_questions:
+                _, created = GovQuestion.objects.get_or_create(
                     submission=submission,
                     question_number=question_number
                 )
