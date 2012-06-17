bfb1f2517bd1153f09f33b6b3b99dad7460adc7b
Adi Eyal <adi@burgercom.co.za>
2012-06-17 13:03:59 +0200

    Adjusted label position

--- ihp/media/js/countryscorecard-d3.js
+++ ihp/media/js/countryscorecard-d3.js
@@ -372,7 +372,7 @@ build_text_element = function(n, id, data, index){
     if (d.text !== undefined){
         g.append('g')
             .attr('class', 'logo')
-            .attr('transform', 'translate(152, 20)')
+            .attr('transform', 'translate(146, 20)')
             .append('text')
             .style('font-size', '20px')
             .style('text-align', 'center')
