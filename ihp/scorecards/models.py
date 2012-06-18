70870a03ff770699657410393418a018302c74b8
Adi Eyal <adi@burgercom.co.za>
2012-06-18 17:18:23 +0200

    reverted to the absolute value for 4g

--- ihp/scorecards/models.py
+++ ihp/scorecards/models.py
@@ -325,11 +325,10 @@ class GovScorecard(object):
                 "description": "Halve the proportion of health sector funding not disbursed against the approved annual budget.",
                 "rating": rating_icon(r4G["target"]),
                 "progress": [
-                    {"year": r4G["base_year"], "value":foz(r4G["one_minus_base_val"])},
+                    {"year": r4G["base_year"], "value":foz(r4G["base_val"])},
                     {"year":"0", "value":0},
-                    {"year": r4G["cur_year"], "value":foz(r4G["one_minus_cur_val"])},
+                    {"year": r4G["cur_year"], "value":foz(r4G["cur_val"])},
                 ],
-                "line": {"constant": 71},
                 "max": 100
             },
             "cipa_scale":{
